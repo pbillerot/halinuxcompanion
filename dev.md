2a15c3575705c7e900547ef1681f34d640e57e54
pbillerot <philippe.billerot@gmail.com>
2024-05-06 09:50:54 +0200

    desktop avec 2 batteries

--- dev.md
+++ dev.md
@@ -3,7 +3,6 @@
 
 
 ## Python
-- sudo apt install pylint3 python-rope python3-pip
 - sudo apt install virtualenv
 - virtualenv --python=/usr/bin/python3 venv
 
@@ -11,12 +10,12 @@
 - (venv) pip install --upgrade pip
 - (venv) pip install -r requirements.txt
 
-- python3 -m halinuxcompanion --config config.json
+- python -m halinuxcompanion --config config.json
 
 ```python
 import datetime as dt
 def get_current_charge():
-     f = open('/sys/class/power_supply/BAT0/charge_now')
+     f = open('/sys/class/power_supply/BAT0/capacity')
      charge, time = round(int(f.read().strip('\n'))/1000), dt.datetime.now().strftime('%H:%M %D') 
      f.close()
      return charge,time 
--- halinuxcompanion/sensors/battery_level.py
+++ halinuxcompanion/sensors/battery_level.py
@@ -1,6 +1,7 @@
 from types import MethodType
 from halinuxcompanion.sensor import Sensor
 import psutil
+import datetime as dt
 
 BatteryLevel = Sensor()
 BatteryLevel.config_name = "battery_level"
@@ -18,7 +19,7 @@ BatteryLevel.unique_id = "battery_level"
 BatteryLevel.unit_of_measurement = "%"
 
 
-def updater(self):
+def updater_old(self):
     data = psutil.sensors_battery()
     if data is not None:
         minutes, seconds = divmod(data.secsleft, 60)
@@ -28,5 +29,22 @@ def updater(self):
         self.icon = "mdi:battery-%d0" % round(data.percent / 10)
         self.attributes["time_left"] = "%d:%02d:%02d" % (hours, minutes, seconds)
 
+def updater(self):
+    try:
+        f = open('/sys/class/power_supply/BAT0/capacity')
+        bat0 = round(int(f.read().strip('\n')))
+        f.close()
+        f = open('/sys/class/power_supply/BAT1/capacity')
+        bat1 = round(int(f.read().strip('\n')))
+        f.close()
+        data = round((bat0 + bat1)/2)
+        data = round(bat0)
+        if data is not None:
+            self.state = data
+            self.icon = "mdi:battery-%d0" % round(data / 10)
+            self.attributes["time_left"] = dt.datetime.now().strftime('%H:%M %D')
+    except Exception as inst:
+        print(inst)
 
 BatteryLevel.updater = MethodType(updater, BatteryLevel)
+
